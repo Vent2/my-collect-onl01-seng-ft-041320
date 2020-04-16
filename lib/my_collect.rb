a5d3c6a5de82cb9cca442fd03ff70e4391dfd0fb
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-16 19:48:50 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
-
-
+def my_collect(arr)
+#   # Set initial i value
+   i = 0
+#   # Create new array to store return values of yield
+   yielded_values = []
+#   #  while loop
+   while i < arr.length
+#     # inside of the while loop shovel in yielded return values into array
+     yielded_values << yield(arr[i])
+#     # increment by one
+     i += 1
+#   end
+   yielded_values
+#   # outside of while loop return array with yielded values
+end
